15ee843e01abf8775a96f6027aa933599604302d
Supragya1 <ssg.gandotra@gmail.com>
2024-02-19 23:08:47 +0530

    Day5

--- Day5/Fizz_Buzz_5.py
+++ Day5/Fizz_Buzz_5.py
@@ -1,5 +1,5 @@
 n = int(input("Enter value of n till where you want to print Fizz Buzz: \n"))
-for i in range(n+1):
+for i in range(1,n+1):
     if(i%3==0 and i%5==0):
         print("FizzBuzz")
     elif(i%3==0):
